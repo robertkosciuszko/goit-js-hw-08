242bfbcaf8f479868295308ffe71cacc3e9e26c7
robertkosciuszko <114183493+robertkosciuszko@users.noreply.github.com>
2023-02-23 13:45:45 +0100

    Uppdate

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime).then(function (time) {});
+
+const timeUpdate = function (seconds) {
+  let time = seconds.seconds;
+  console.log(time);
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
 
